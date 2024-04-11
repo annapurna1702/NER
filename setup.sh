de0b78da523a862acdcbe457b21693b938cb5f91
Annapurna Padmanabhan <108138866+annapurna1702@users.noreply.github.com>
2024-04-11 13:27:37 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,6 @@
-#!/bin/bash
 
-# Update package lists
-sudo apt-get update
-
-# Install Python and pip
-sudo apt-get install python3 python3-pip -y
-
-# Install spaCy and its language models
 pip install spacy
 python -m spacy download en_core_web_sm
 
-# Install other dependencies specified in requirements.txt
-pip install -r requirements.txt
 
-# Run the Streamlit app
-streamlit run assignment1.py
 
